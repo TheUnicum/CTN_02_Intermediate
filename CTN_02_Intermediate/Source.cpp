71a036ea99da849cf5284aa1d254ddda3b48d6a3
TheUnicum <tia_ben@tin.it>
2019-12-09 11:03:14 +0100

    Example 5 (26:00)

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -4,9 +4,15 @@ int main()
 	int arr[4] = { 10, 11, 12, 13 };
 	
 	int* pInt = &arr[1];
+	*arr = 4;
 
 	*pInt = 69;
+	pInt[0] = 70;
 	pInt[2] = 69;
+
+
+	int sizep = sizeof(pInt);
+	int sizes = sizeof(arr);
 
 	return 0;
 }

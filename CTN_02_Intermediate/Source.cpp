d17148973d2182e1f6f7af50bcd94817d334da81
TheUnicum <tia_ben@tin.it>
2019-12-09 10:39:23 +0100

    Example 2 (12:45)

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,11 +1,49 @@
+
+void Swap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void SwapP(int* pa, int* pb)
+{
+	int temp = *pa;
+	*pa = *pb;
+	*pb = temp;
+}
+
+void SwapR(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
 int main()
 {
+	int m = 7;
 	int n = 5;
-	 
-	int* p = &n;
-	*p = *p + 64;
+	Swap(m, n);		//WRONG!!!!!!!!!!!!!!
+	SwapP(&m, &n);	//OK passing by pointer!
+	SwapR(m, n);	//OK passing by reference!
 
-	int* q = p;
+
+	// pointer-----------------------------------------------
+	// int
+	int* ptr = &m;
+	// can also declare without init
+	int* ptr2;
+	// can retarget
+	ptr2 = &n;
+	// or accessing to poiter to
+	*ptr2 = 22;
+
+	// ref---------------------------------------------------
+	int& ref = m;
+	// int& ref2; error! cannot declare without init
+	// cannot retarget!
+	ref = n; //WARNONG!!! this code store the value of n in m!
 
 	return 0;
 }

5d00e858f51f753ceca65d4c74768d0fec3b136c
TheUnicum <tia_ben@tin.it>
2019-12-10 12:02:14 +0100

    Example 1

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,21 @@
+#include <conio.h>
+
 int main()
 {
-	// Hello There!!
+	// Char literal
+	_putch('P');
+	_putch('u');
+	_putch('b');
+	_putch('e');
+	_putch('s');
 
+	_putch(80);
+	_putch(117);
+	_putch(98);
+	_putch(101);
+	_putch(115);
+	_putch(33);
+
+	while (!_kbhit());
 	return 0;
 }

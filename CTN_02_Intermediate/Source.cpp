7832e0c14fe5c9117e2b4c32442448d4e56dd5e0
TheUnicum <tia_ben@tin.it>
2019-12-11 20:19:13 +0100

    Intro to Vector, Construcor / Size ..

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+
 int main()
 {
-	// Hello There!!
+	std::cout << "How many? ";
+	int size;
+	std::cin >> size;
 
+	std::vector<int> vec1(size);
+
+	std::cout <<"\nYour vector contains:";
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << " " << vec1[i];
+	}
+
+	std::vector<int> vec2(size, 69);
+	std::cout << "\nYour vector contains:";
+	for (int i = 0; i < vec2.size(); i++)
+	{
+		std::cout << " " << vec2[i];
+	}
+
+	std::vector<int> vec3({69, 420, size});
+	std::cout << "\nYour vector contains:";
+	for (const int n : vec3)
+	{
+		std::cout << " " << n;
+	}
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cin.get();
 	return 0;
 }

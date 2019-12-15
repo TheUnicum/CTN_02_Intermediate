55c99332627f3a2318f639edb98a8240e8d24cfa
TheUnicum <tia_ben@tin.it>
2019-12-15 16:46:47 +0100

    [T16] (0:0) - Sort a vector

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,18 @@
-int main()
-{
-	// Hello There!!
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+int main()
+{	//https://en.cppreference.com/w/cpp/algorithm/sort
+	std::vector<int> vi = { 3,0,1,9,2,4,6,1,8,7,5 };
+
+	std::sort(vi.begin(), vi.end());
+
+	for (int n : vi)
+	{
+		std::cout << n;
+	}
+
+	std::cin.get();
 	return 0;
 }

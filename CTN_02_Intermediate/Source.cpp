f647d6ee15ee04b1ec8b81f4ef088c9c4594f19e
TheUnicum <tia_ben@tin.it>
2019-12-15 16:49:49 +0100

    [T16] (3:09) Sort compare, pass a functor

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 int main()
-{	//https://en.cppreference.com/w/cpp/algorithm/sort
+{	//	https://en.cppreference.com/w/cpp/algorithm/sort
+	//	https://en.cppreference.com/w/cpp/header/functional
 	std::vector<int> vi = { 3,0,1,9,2,4,6,1,8,7,5 };
 
-	std::sort(vi.begin(), vi.end());
+	std::sort(vi.begin(), vi.end(), std::greater<int>{});
 
 	for (int n : vi)
 	{

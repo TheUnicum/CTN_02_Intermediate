7bdd0017075710b2c498a4db976f79c176ff6f7c
TheUnicum <tia_ben@tin.it>
2019-12-15 15:43:39 +0100

    [T15] - (10:55) erase returns iterator

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <vector>
 
-class Pubes
-{
-public:
-	int x;
-};
-
 int main()
-{
-	// https://en.cppreference.com/w/cpp/container/vector/erase
+{	// https://en.cppreference.com/w/cpp/container/vector/erase
 	std::vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7 };
 
-	// ERASE
-	//v.erase(v.begin() + 3);
-	//v.erase(v.begin() + 3, v.begin() + 6);
-	
-	//
-	auto i_invalidated = v.begin() + 4;
-	//v.erase(v.begin() + 3, v.begin() + 6);
-	v.push_back(69);
-	std::cout << *i_invalidated;
+	for (auto i = v.begin(), e = v.end(); i != e; ++i)
+	{
+		// erase all multiple of 3
+		if (*i % 3 == 0)
+		{
+			i = v.erase(i);
+		}
+		else
+		{
+			++i;
+		}
+	}
 
 	for (int val : v)
 	{

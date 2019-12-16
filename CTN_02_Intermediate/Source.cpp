5308abdf2979dfe8418c398a198daaa01c3cda68
TheUnicum <tia_ben@tin.it>
2019-12-16 15:36:45 +0100

    [T17.1] - (0:0) Example 1 <public Parent>

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,35 @@
+#include <iostream>
+#include <string>
+
+class A
+{
+public:
+	void FuncX()
+	{}
+private:
+	int var1;
+};
+
+// use ": public Parent" to inherit from another class
+class B : public A
+{
+public:
+	void FuncY()
+	{}
+private:
+	int var2;
+};
+
 int main()
 {
-	// Hello There!!
+	A mya;
+	B myb;
 
+	myb.FuncX();
+
+	std::cout << sizeof(mya) << std::endl;
+	std::cout << sizeof(myb) << std::endl;
+
+	std::cin.get();
 	return 0;
 }

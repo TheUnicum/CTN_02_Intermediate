b7910e01fb05fd1a89fce64647b65c4fb1fbd746
TheUnicum <tia_ben@tin.it>
2019-12-16 18:31:23 +0100

    [T18.1] - (14:25) <virtual destructor>!!!!!

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -55,7 +55,10 @@ public:
 		}
 	}
 	virtual void SpecialMove(MemeFighter&) = 0;
-
+	virtual ~MemeFighter()
+	{
+		std::cout << "Destroying MemeFighter '" << name << "'!" << std::endl;
+	}
 protected:
 	MemeFighter(const std::string& name, int hp, int speed, int power)
 		:
@@ -119,6 +122,10 @@ public:
 			MemeFighter::Tick();
 		}
 	}
+	~MemeFrog()
+	{
+		std::cout << "Destroying MemeFrog '" << name << "'!" << std::endl;
+	}
 };
 
 class MemeStoner : public MemeFighter
@@ -145,6 +152,10 @@ public:
 				std::cout << GetName() << " spaces out." << std::endl;
 			}
 		}
+	}
+	~MemeStoner()
+	{
+		std::cout << "Destroying MemeStoner '" << name << "'!" << std::endl;
 	}
 };
 
@@ -230,7 +241,6 @@ int main()
 	{
 		std::cout << "Team TWO is victorious!" << std::endl;
 	}
-	while (!_kbhit());
 
 	for (size_t i = 0; i < t1.size(); i++)
 	{
@@ -238,5 +248,6 @@ int main()
 		delete t2[i];
 	}
 
+	while (!_kbhit());
 	return 0;
 }

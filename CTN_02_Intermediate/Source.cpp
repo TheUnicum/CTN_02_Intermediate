31998dc28325a0027ec2cc048661a9be08f621e4
TheUnicum <tia_ben@tin.it>
2019-12-17 18:11:23 +0100

    [T20] - (5:35) - && operator for rvalue paramiter.

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -4,6 +4,8 @@
 
 void nomnomnom(const std::string& str)
 {
+	std::cout << "lval noming!\n";
+
 	auto copy = str;
 	for (auto& c : copy)
 	{
@@ -12,11 +14,24 @@ void nomnomnom(const std::string& str)
 	std::cout << "Nom nom nom: " << copy << std::endl;
 }
 
+void nomnomnom(std::string&& donor)
+{
+	std::cout << "rval noming!\n";
+
+	for (auto& c : donor)
+	{
+		c++;
+	}
+	std::cout << "Nom nom nom: " << donor << std::endl;
+}
+
 int main()
 {
 	std::string s = "This is just a dumb string pay it no heed.";
 	nomnomnom(s);
 	std::cout << "after noming: " << s << std::endl;
+
+	nomnomnom(std::string("here is adumb string, it is going to die soon"));
 
 	std::cin.get();
 	return 0;

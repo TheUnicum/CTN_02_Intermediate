c2a4fb8972caf0a7b1284d973f6d5450b32af611
TheUnicum <tia_ben@tin.it>
2019-12-17 22:16:25 +0100

    [T21] - (2:37) - std::make_unique.

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,61 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Pube
+{
+public:
+	Pube()
+	{
+		std::cout << "pube ctor\n";
+	}
+	Pube(const Pube&)
+	{
+		std::cout << "pube copy ctor\n";
+	}
+	~Pube()
+	{
+		std::cout << "pube dtor\n";
+	}
+	void mint() const
+	{
+		std::cout << "newt!\n";
+	}
+};
+
+void f(const Pube p)
+{
+	std::cout << "f(): ";
+	p.mint();
+}
+
+void g(const Pube& p)
+{
+	std::cout << "g(): ";
+	p.mint();
+}
+
+void h(const Pube* pp)
+{
+	std::cout << "h(): ";
+	pp->mint();
+}
+
 int main()
 {
-	// Hello There!!
+	{
+		auto p = std::make_unique<Pube>();
+		f(*p);
+		g(*p);
+		h(p.get());
+		p->mint();
+	}
 
+	std::cout << "------\n";
+	{
+		auto p = std::make_unique<Pube[]>(3);
+		p[2].mint();
+	}
+	std::cin.get();
 	return 0;
 }

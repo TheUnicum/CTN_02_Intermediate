94c6fd9e60bfad4ffc9db6585ac6a2902d67720f
TheUnicum <tia_ben@tin.it>
2019-12-17 18:03:51 +0100

    [T20] - (3:07) - rvalue & lvalue.

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class A
+{
+public:
+	int& Get()
+	{
+		return x;
+	}
+private:
+	int x;
+};
+
+int f()
+{
+	return 69;
+}
+
 int main()
 {
-	// Hello There!!
+	// rvalue can only appear on right side of assigment (=)
+	// literal int
+	420 = 69;
+	// temporary return value from function
+	f() = 420;
+
+	// lvalue can be
+	int x;
+	x = 69;
+
+	A obj;
+	obj.Get() = 69;
+
+	int r[5];
+	r[3] = 420;
 
 	return 0;
 }

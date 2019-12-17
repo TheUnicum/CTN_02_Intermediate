f95cb90ea4a5d929e3af30c67d9c6694552900d2
TheUnicum <tia_ben@tin.it>
2019-12-17 18:18:31 +0100

    [T20] - (9:05) - std::move on std::string.

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -2,37 +2,12 @@
 #include <string>
 #include <vector>
 
-void nomnomnom(const std::string& str)
-{
-	std::cout << "lval noming!\n";
-
-	auto copy = str;
-	for (auto& c : copy)
-	{
-		c++;
-	}
-	std::cout << "Nom nom nom: " << copy << std::endl;
-}
-
-void nomnomnom(std::string&& donor)
-{
-	std::cout << "rval noming!\n";
-
-	for (auto& c : donor)
-	{
-		c++;
-	}
-	std::cout << "Nom nom nom: " << donor << std::endl;
-}
-
 int main()
 {
-	std::string s = "This is just a dumb string pay it no heed.";
-	//nomnomnom(static_cast<std::string&&>(s));
-	nomnomnom(std::move(s));
-	std::cout << "after noming: " << s << std::endl;
+	std::string a = "i am a very good bad boy";
+	std::string b = std::move(a);// a;
 
-	nomnomnom(std::string("here is adumb string, it is going to die soon"));
+	std::cout << "a: " << a << std::endl << "b: " << b << std::endl;
 
 	std::cin.get();
 	return 0;

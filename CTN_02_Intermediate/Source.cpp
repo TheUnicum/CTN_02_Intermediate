e478137557ca7d4eb08189517eb3e8939af12a69
TheUnicum <tia_ben@tin.it>
2019-12-18 22:05:15 +0100

    [T25] - (0:0) Switch to a functor.

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -4,17 +4,22 @@
 #include <conio.h>
 #include <memory>
 
-struct SixtyNine
+struct CaseFunction
 {
-	void operator()() const
+	virtual void operator()() const = 0;
+};
+
+struct SixtyNine : public CaseFunction
+{
+	void operator()() const override
 	{
 		std::cout << "the sex number";
 	}
 };
 
-struct FourTwenty
+struct FourTwenty : public CaseFunction
 {
-	void operator()() const
+	void operator()() const override
 	{
 		std::cout << "the weed number";
 	}
@@ -22,6 +27,13 @@ struct FourTwenty
 
 int main()
 {
+	std::unordered_map<std::string, std::unique_ptr<CaseFunction>> sw;
 
+	sw["sixty-nine"] = std::make_unique<SixtyNine>();
+	sw["four-twenty"] = std::make_unique<FourTwenty>();
+
+	(*sw["sixty-nine"])();
+
+	while (!_kbhit());
 	return 0;
 }

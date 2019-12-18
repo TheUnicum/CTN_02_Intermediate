131c47caba6e3aca9ff65e149045571811f1063a
TheUnicum <tia_ben@tin.it>
2019-12-18 22:15:14 +0100

    [T25] - (5:13) store a function in a variable , type <int(*pFuncName)(int)>

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -4,35 +4,53 @@
 #include <conio.h>
 #include <memory>
 
-struct CaseFunction
-{
-	virtual void operator()() const = 0;
-};
+//struct CaseFunction
+//{
+//	virtual void operator()() const = 0;
+//};
+//
+//struct SixtyNine : public CaseFunction
+//{
+//	void operator()() const override
+//	{
+//		std::cout << "the sex number";
+//	}
+//};
+//
+//struct FourTwenty : public CaseFunction
+//{
+//	void operator()() const override
+//	{
+//		std::cout << "the weed number";
+//	}
+//};
 
-struct SixtyNine : public CaseFunction
+int Double(int x)
 {
-	void operator()() const override
-	{
-		std::cout << "the sex number";
-	}
-};
+	return x * 2;
+}
 
-struct FourTwenty : public CaseFunction
+int Triple(int x)
 {
-	void operator()() const override
-	{
-		std::cout << "the weed number";
-	}
-};
+	return x * 3;
+}
 
 int main()
 {
-	std::unordered_map<std::string, std::unique_ptr<CaseFunction>> sw;
+	//std::unordered_map<std::string, std::unique_ptr<CaseFunction>> sw;
 
-	sw["sixty-nine"] = std::make_unique<SixtyNine>();
-	sw["four-twenty"] = std::make_unique<FourTwenty>();
+	//sw["sixty-nine"] = std::make_unique<SixtyNine>();
+	//sw["four-twenty"] = std::make_unique<FourTwenty>();
 
-	(*sw["sixty-nine"])();
+	//(*sw["sixty-nine"])();
+
+	int(*pFunc)(int);
+
+	pFunc = Double;
+	std::cout << pFunc(2) << std::endl;
+
+	pFunc = Triple;
+	std::cout << pFunc(2) << std::endl;
 
 	while (!_kbhit());
 	return 0;

d92b05d4e102536c80fc24348eb6a8b802d73c14
TheUnicum <tia_ben@tin.it>
2019-12-18 15:04:16 +0100

    [T21.1] - (21:57) - END - [Exceptions] Tutorial 22.1

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,30 +1,95 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
-class Pube
+/*
+// Rules
+1.
+throw by value
+catch by reference
+2.
+Never use as control flow
+*/
+
+class Annuncer
 {
 public:
-	Pube()
+	Annuncer(const std::string& name)
+		:
+		name(name)
 	{
-		std::cout << "constructing pube" << std::endl;
+		std::cout << "Constructing " << name << std::endl;
 	}
-	Pube(Pube&&) noexcept
+	~Annuncer()
 	{
-		std::cout << "moving pube" << std::endl;
+		std::cout << "Destroying " << name << std::endl;
+		throw std::runtime_error("Oops");
 	}
-	Pube(const Pube&)
-	{
-		std::cout << "copying pube" << std::endl;
-	}
+private:
+	std::string name;
 };
+
+int sum(int a, int b)
+{
+	Annuncer j("j");
+	Annuncer k("k");
+
+	auto p = std::make_unique<Annuncer>("fuck");
+
+	if (a == 69)
+	{
+		throw std::runtime_error("<a> is the sex number!");
+	}
+
+	if (b == 420)
+	{
+		throw std::logic_error("<b> is the weed number!");
+	}
+
+	Annuncer l("l");
+
+	return a + b;
+}
+
+int f(int x, int y, int z)
+{
+	try
+	{
+		return sum(x, y) * sum(y, z);
+	}
+	catch (const std::runtime_error & e)
+	{
+		std::cout << "Caught a std::runtime_error in f: " << e.what() << std::endl;
+		throw;
+	}
+}
 
 int main()
 {
-	std::vector<Pube> pubes;
-	pubes.emplace_back();
-	pubes.emplace_back();
-	pubes.emplace_back();
+	try
+	{
+		std::cout << f(69, 420, 7) << std::endl;
+	}
+	catch (const std::runtime_error & e)
+	{
+		std::cout << "Caught a std::runtime_error in main: " << e.what() << std::endl;
+	}
+	catch (const std::exception & e)
+	{
+		std::cout << "Caught a std::exception in main: " << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "Caught *something* in main" << std::endl;
+		//throw;
+	}
 
 	std::cin.get();
 	return 0;
 }
+
+/*
+http://download.vikis.lt/doc/libstdc++-docs-4.4.7/html/api/a00367.html
+
+https://en.cppreference.com/w/cpp/error/logic_error
+https://en.cppreference.com/w/cpp/error/runtime_error
+*/

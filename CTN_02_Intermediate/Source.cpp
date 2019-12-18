1a8753727d0abb5f4893ba362b648e09de8c858e
TheUnicum <tia_ben@tin.it>
2019-12-18 17:09:52 +0100

    [T24.1] - (2:46) - some map examples...

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -10,9 +10,13 @@ int main()
 	std::map<std::string, std::string> map;
 
 	// initialize the map
-	map.insert({ "crabdance", "crab people, crab people!" });
+	map.insert(std::pair<std::string, std::string>{ "crabdance", "crab people, crab people!" });
+	map.emplace("asdfasdf", "asdfasdf asdfasdf asdf adsf");
 	map.insert({ "cashme", "owsigh, hawbadah?" });
 	map.insert({ "so", "you're saying that babies are delicious, and their flesh should be consumed daily" });
+
+	map.count("so");
+	std::cout << map.find("so")->second << std::endl;
 
 	std::cin >> command;
 	std::cout << map[command] << std::endl;
@@ -23,4 +27,6 @@ int main()
 
 /*
 https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
+
+https://it.cppreference.com/w/cpp/container/map
 */

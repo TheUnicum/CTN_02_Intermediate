0ad78994ae22f1d72b4fd47a895f3f13a6563da9
TheUnicum <tia_ben@tin.it>
2019-12-18 16:56:19 +0100

    [T24.1] - (0:45) - std::map

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <string>
 #include <conio.h>
+#include <map>
 
 int main()
 {
 	std::string response;
 	std::string command;
+	std::map<std::string, std::string> map;
+
+	// initialize the map
+	map.insert({ "crabdance", "crab people, crab people!" });
+	map.insert({ "cashme", "owsigh, hawbadah?" });
+	map.insert({ "so", "you're saying that babies are delicious, and their flesh should be consumed daily" });
 
 	std::cin >> command;
-	switch (command)
-	{
-	case "crabdance":
-		response = "crab people, crab people!";
-		break;
-	// etc.
-	}
-
-	std::cout << response << std::endl;
+	std::cout << map[command] << std::endl;
 
 	while (!_kbhit());
 	return 0;
 }
+
+/*
+https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
+*/

9aa3a3dab5ae79ebea4fb6e35c24405a14d7ae88
TheUnicum <tia_ben@tin.it>
2019-12-18 14:18:01 +0100

    [T21.1] - (5:09) - Propagate through functions.

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
+int sum(int a, int b)
+{
+	if (a == 69)
+	{
+		throw std::runtime_error("<a> is the sex number!");
+	}
+
+	return a + b;
+}
+
+int f(int x, int y, int z)
+{
+	return sum(x, y) * sum(y, z);
+}
+
 int main()
 {
 	try
 	{
-		throw std::runtime_error("An exception!");
+		std::cout << f(1,69, 7) << std::endl;
 	}
 	catch (const std::runtime_error& e)
 	{

e546b40b7be385e3a32e74204f928b53e99bdac7
TheUnicum <tia_ben@tin.it>
2019-12-18 15:41:00 +0100

    [T23] --- INIT ----

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,25 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+std::string ToBin(unsigned int n, int min_digits = 0)
+{
+	std::string bin_str;
+	for (int count = 0; n != 0 || count < min_digits; n >>= 1, count++)
+	{
+										// this ? : is called the 'ternary' opertor BTW
+		bin_str.push_back(bool(n & 0b1) ? '1' : '0');
+	}
+	std::reverse(bin_str.begin(), bin_str.end());
+	return bin_str;
+}
+
 int main()
 {
-	// Hello There!!
+	std::cout << ToBin(232) << std::endl;
+	std::cout << ToBin(0xE8) << std::endl;
+	std::cout << ToBin(0b11101000) << std::endl;
 
+	std::cin.get();
 	return 0;
 }

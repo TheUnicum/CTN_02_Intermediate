0719b9c1d89d3af362f077ee262573e6dac471e7
TheUnicum <tia_ben@tin.it>
2019-12-18 14:57:00 +0100

    [T21.1] - (18:34) - form VS2017 use  nowexcept to enable Move-ass. or just says to compiler function will never throw an exception.

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,86 +1,30 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
-class Annuncer
+class Pube
 {
 public:
-	Annuncer(const std::string& name)
-		:
-		name(name)
+	Pube()
 	{
-		std::cout << "Constructing " << name << std::endl;
+		std::cout << "constructing pube" << std::endl;
 	}
-	~Annuncer()
+	Pube(Pube&&) noexcept
 	{
-		std::cout << "Destroying " << name << std::endl;
-		throw std::runtime_error("Oops");
+		std::cout << "moving pube" << std::endl;
 	}
-private:
-	std::string name;
+	Pube(const Pube&)
+	{
+		std::cout << "copying pube" << std::endl;
+	}
 };
-
-int sum(int a, int b)
-{
-	Annuncer j("j");
-	Annuncer k("k");
-
-	auto p = std::make_unique<Annuncer>("fuck");
-
-	if (a == 69)
-	{
-		throw std::runtime_error("<a> is the sex number!");
-	}
-
-	if (b == 420)
-	{
-		throw std::logic_error("<b> is the weed number!");
-	}
-
-	Annuncer l("l");
-
-	return a + b;
-}
-
-int f(int x, int y, int z)
-{
-	try
-	{
-		return sum(x, y) * sum(y, z);
-	}
-	catch (const std::runtime_error& e)
-	{
-		std::cout << "Caught a std::runtime_error in f: " << e.what() << std::endl;
-		throw;
-	}
-}
 
 int main()
 {
-	try
-	{
-		std::cout << f(69,420, 7) << std::endl;
-	}
-	catch (const std::runtime_error& e)
-	{
-		std::cout << "Caught a std::runtime_error in main: " << e.what() << std::endl;
-	}
-	catch (const std::exception & e)
-	{
-		std::cout << "Caught a std::exception in main: " << e.what() << std::endl;
-	}
-	catch (...)
-	{
-		std::cout << "Caught *something* in main" << std::endl;
-		//throw;
-	}
+	std::vector<Pube> pubes;
+	pubes.emplace_back();
+	pubes.emplace_back();
+	pubes.emplace_back();
 
 	std::cin.get();
 	return 0;
 }
-
-/*
-http://download.vikis.lt/doc/libstdc++-docs-4.4.7/html/api/a00367.html
-
-https://en.cppreference.com/w/cpp/error/logic_error
-https://en.cppreference.com/w/cpp/error/runtime_error
-*/

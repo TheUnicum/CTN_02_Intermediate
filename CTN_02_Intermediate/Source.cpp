fc806ee0635069e1cbf2792fecfc10794001b81b
TheUnicum <tia_ben@tin.it>
2019-12-18 16:45:24 +0100

    [T24.1] ---- INIT-----

--- CTN_02_Intermediate/Source.cpp
+++ CTN_02_Intermediate/Source.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+
 int main()
 {
-	// Hello There!!
+	std::string response;
+	std::string command;
 
+	std::cin >> command;
+	switch (command)
+	{
+	case "crabdance":
+		response = "crab people, crab people!";
+		break;
+	// etc.
+	}
+
+	std::cout << response << std::endl;
+
+	while (!_kbhit());
 	return 0;
 }
